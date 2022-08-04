038496f2a82e29ce8dbf1a8aa2d6391c45ffbf78
David Romero Scozziero <davidfromeroscozziero@hotmail.com>
2022-08-04 19:10:05 -0300

    Actualizacion 04/08/22

--- PortfolioDRS/Frontend/src/app/app.module.ts
+++ PortfolioDRS/Frontend/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AcercaDComponent } from './components/acerca-d/acerca-d.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoargpComponent } from './components/logoargp/logoargp.component';
-import { SocialComponent } from './components/social/social.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
@@ -22,7 +21,6 @@ import { FooterComponent } from './components/footer/footer.component';
     BannerComponent,
     HeaderComponent,
     LogoargpComponent,
-    SocialComponent,
     ExperienciaComponent,
     EducacionComponent,
     SkillsComponent,
