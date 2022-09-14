c5450a13fde28ddfed776aa72b1b24666a6df2c1
David Romero Scozziero <davidfromeroscozziero@hotmail.com>
2022-09-13 23:01:09 -0300

    Actualización 13/09/22

--- PortfolioDRS/Frontend/src/app/app.module.ts
+++ PortfolioDRS/Frontend/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion/editeducacion.component';
  
 @NgModule({
   declarations: [
@@ -34,7 +36,9 @@ import { EditExperienciaComponent } from './components/experiencia/edit-experien
     HomeComponent,
     LoginComponent,
     NewExperienciaComponent,
-    EditExperienciaComponent
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent
 
   ],
   imports: [
