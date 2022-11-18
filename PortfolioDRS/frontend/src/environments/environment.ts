f5354d9168d4eac4f8040d2934b898310836477d
David Romero Scozziero <99300731+davidrscozziero@users.noreply.github.com>
2022-11-18 17:37:46 -0300

    upload environment.ts

---
+++ PortfolioDRS/frontend/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'portfoliodrs',
+    appId: '1:674012480471:web:028bf6bb094afcbf91c192',
+    storageBucket: 'portfoliodrs.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCA9Ezls6GQwE2Jv-Qqyes1heWOiaMO6as',
+    authDomain: 'portfoliodrs.firebaseapp.com',
+    messagingSenderId: '674012480471',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
